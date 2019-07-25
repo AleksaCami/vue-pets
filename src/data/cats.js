e8c85a282bd0e698dc90dd9e42b1b8f22924728a
AleksaCamilovic <aleksa.camilovic@outlook.com>
2019-07-25 14:50:32 +0200

    initial commit

---
+++ src/data/cats.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    name: 'Mirko',
+    breed: 'corgi',
+    species: 'cat',
+    gender: 'z',
+    age: '7',
+    color: 'orange',
+    weight: '10',
+    location: 'Beograd',
+    notes: 'Pure breed, Trained for competitions'
+  },
+  {
+    name: 'Mirko',
+    breed: 'corgi',
+    species: 'cat',
+    gender: 'z',
+    age: '7',
+    color: 'orange',
+    weight: '10',
+    location: 'Beograd',
+    notes: 'Pure breed, Trained for competitions'
+  },
+  {
+    name: 'Mirko',
+    breed: 'corgi',
+    species: 'cat',
+    gender: 'z',
+    age: '7',
+    color: 'orange',
+    weight: '10',
+    location: 'Beograd',
+    notes: 'Pure breed, Trained for competitions'
+  }
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'Mirko',
+    breed: 'corgi',
+    gender: 'z',
+    age: '7',
+    color: 'orange',
+    weight: '10',
+    location: 'Beograd',
+    notes: 'Pure breed, Trained for competitions'
+  },
+  {
+    name: 'Mirko',
+    breed: 'corgi',
+    gender: 'z',
+    age: '7',
+    color: 'orange',
+    weight: '10',
+    location: 'Beograd',
+    notes: 'Pure breed, Trained for competitions'
+  },
+  {
+    name: 'Mirko',
+    breed: 'corgi',
+    gender: 'z',
+    age: '7',
+    color: 'orange',
+    weight: '10',
+    location: 'Beograd',
+    notes: 'Pure breed, Trained for competitions'
+  }
+]
